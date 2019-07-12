1a4e82e9211cb4ab944635ffb169e862eac661ad
Aditya Naik <naik.aditya23@gmail.com>
2019-07-12 15:59:29 -0400

    commented out checksum stuff which does not work

--- src/datagram/datagram.h
+++ src/datagram/datagram.h
@@ -4,40 +4,49 @@
 
 struct cmd{
     unsigned int cmd_num, rw, reg, data;
+    char checksum;
 };
 
-int parseCommand(struct cmd *_cmd, char cmd[14]) {
-    char *c_num, *c_rw, *c_reg, *c_data, *endptr;
+int parseCommand(struct cmd *_cmd, char cmd[13]) {
+    char *c_num, *c_rw, *c_reg, *c_data, *endptr, *checksum;
     c_num = malloc(2);
     c_rw = malloc(2);
     c_reg = malloc(5);
     c_data = malloc(4);
+    checksum = malloc(2);
     
     int num, rw, reg, data, offset = 0;
 
     strncpy(c_num, cmd, 2);
     num=(int)strtol(c_num, NULL, 16);
     offset+=2;
-    strncpy(c_rw, cmd+offset, 2);
+    
+    strncpy(c_rw, cmd+offset, 1);
     rw = (int)strtol(c_rw, NULL, 16);
-    offset+=2;
+    offset+=1;
 	
     strncpy(c_reg, cmd+offset, 4);
     reg = strtol(c_reg, &endptr, 16);
     offset+=4;
 	
-    strcpy(c_data, cmd+offset);
+    strncpy(c_data, cmd+offset, 4);
     data = (int)strtol(c_data, NULL, 16);
-    offset+=2;
+    offset+=4;
 
+    /* strcpy(checksum, cmd+offset); */
+    
     _cmd->cmd_num = num;
     _cmd->rw = rw;
     _cmd->reg = reg;
     _cmd->data = data;
+    /* _cmd->checksum = *checksum; */
+    
     free(c_num);
     free(c_rw);
     free(c_reg);
     free(c_data);
+    free(checksum);
+    
     return 1;
 }
 
@@ -49,8 +58,7 @@ int getCommand(char *cmd_out, int cmd_num, int rw_in, int reg_in, int data_in) {
     offset+=sprintf(cmd+offset, "%x", rw_in);
     offset+=sprintf(cmd+offset, "%x", reg_in);
     
-    if(rw_in == 1)
-	offset+=sprintf(cmd+offset, "%04x", data_in);
+    offset+=sprintf(cmd+offset, "%04x", data_in);
     
     strcpy(cmd_out, cmd);
 }
