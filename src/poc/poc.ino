0f966f3259d7784b18a3711515beaf3a0fd13f41
Aditya Naik <naik.aditya23@gmail.com>
2019-07-19 18:46:15 -0400

    added o2 refactoring to poc

--- src/poc/poc.ino
+++ src/poc/poc.ino
@@ -1,30 +1,16 @@
 
-#include <ModbusMaster.h>
-#include <SoftwareSerial.h>
-#include <Wire.h>
-#include <string.h>
+
+
+#include "o2.h"
+#include "reg.h"
+#include "datagram.h"
 #include "hsc_ssc_i2c.h"
+#include <Wire.h>
 
-#define NUM_SENSORS 4
-#define BAUD 9600
-
-// SST Registers
-// Input regs 
-#define O2AVG_REG  30001
-#define STATUS_REG 30004
-#define ERR_REG    30005
-#define CALSTS_REG 30018
-// Holding regs
-#define ONOFF_REG   40001
-#define CLCTRL_REG  40004 // Calibration control
-#define ADDR_REG    40006
-#define ERR_CLR_REG 40002
-
+//=============================Pressure Vars============================
 #define MAX_SENSORS 6
-
 // Constant mux address
 #define MUXADDR 0x70
-
 // see hsc_ssc_i2c.h for a description of these values
 // these defaults are valid for the HSCMRNN030PA2A3 chip
 #define SLAVE_ADDR 0x28
@@ -34,121 +20,28 @@
 #define P_MAX0 1
 #define P_MIN1 -5
 #define P_MAX1 5
+//======================================================================
 
-// Starting PIN on the microcontroller
-const static unsigned int START_PIN PROGMEM = 4;
-// Delay for periodic checks (status, calibration et cetera)
-const static unsigned int CHK_DELAY = 20;
 
-// System status
-const static int IDLE     PROGMEM = 0;
-const static int STARTUP  PROGMEM = 1;
-const static int ON       PROGMEM = 2;
-const static int SHUTDOWN PROGMEM = 3;
-const static int STANDBY  PROGMEM = 4;
-
-// Calibration status codes
-const static int CAL_IDLE PROGMEM = 0;
-const static int CAL_PROG PROGMEM = 1;
-const static int CAL_DONE PROGMEM = 2;
-
-// Flag codes
-const static int FLAG_NONE PROGMEM = 0;
-const static int FLAG_CAL  PROGMEM = 1;
-const static int FLAG_DONE PROGMEM = 2;
-const static int FLAG_OFF  PROGMEM = 3;
-
-uint32_t prev = 0;
-const uint32_t interval = 10;
-
-SoftwareSerial modbus(4, 5);
-ModbusMaster *node = malloc(sizeof(ModbusMaster)*4);
-
-/*
-  Status < 0 means there's an active error code in err[] for the sensor
-  Error == 0 means that there's sensor status value = valid
-
-  flag[] tracks the calibration demands from the interrupt pins,
-  while cal tracks the actual calibration state of the boards
-  flag[i] == FLAG_CAL means that calibration is in process for sensor i
-  this calibration could be DONE, PROG or IDLE.. cal[i] would hold
-  that info
-*/
-int status[NUM_SENSORS], cal[NUM_SENSORS], flag[NUM_SENSORS] = {FLAG_NONE};
-
-/*
-
-  DONE parse protocol errors
-
-  Response codes
-
-  0  Success
-  1 invalid response slave ID exception
-  2 invalid response function exception
-  3 response timed out exception
-  4 invalid response CRC exception
-  5 modbus proto illegal func
-  6 modbus proto illegal data address
-  7 modbus proto illegal data value
-  8 modbus proto device failure
-
-  4-20ma.io/ModbusMaster/group__constant.html#gaf69c1c360b84adc5bf1c7bbe071f1970
-
-  This func expects int representations of hex error values and converts them 
-  into errors in [-1, -8]
-*/
-int parseError(int e){
-    int ret;
-    if(e >= 224 && e <= 227)
-	ret = 0-(e % 223);
-    else if(e >=1 && e <= 4)
-	ret = 0-(e+4);
-    return ret;
-}
-
-int readReg(int sensor, int reg){
-    int res;
-    int val;
-    res = node[sensor].readInputRegisters(reg, 1);
-    if(res == 0){
-	val = node[sensor].getResponseBuffer(0);
-	node[sensor].clearResponseBuffer();
-    }
-    else{
-	return parseError(res);
-    }
-    return val;
-}
-
-// If this func is taking an int parameter for value, can it be used for calibration?
-int writeReg(int sensor, int reg, int value){
-    int res, ret;
-    res = node[sensor].writeSingleRegister(reg, value);
-    if(res == 0)
-	ret = res;
-    else
-	ret = parseError(res);
-    return ret;
-}
-
+//=============================Oxygen Funcs=============================
 void handleSensor(int i){
     int res, cal_res;
-    status[i] = readReg(i, STATUS_REG); delay(4);
-    cal[i] = readReg(i, CALSTS_REG); delay(4);
+    status[i] = readReg(i, STATUS_REG, node); delay(4);
+    cal[i] = readReg(i, CALSTS_REG, node); delay(4);
     
     if(flag[i] == FLAG_CAL && cal[i] == CAL_IDLE){ // Calibrate
-	res = writeReg(i, CLCTRL_REG, 1); delay(4);
+	res = writeReg(i, CLCTRL_REG, 1, node); delay(4);
 	if(res < 0)
 	    status[i] = res;
 	else
-	    cal[i] = readReg(i, CALSTS_REG); delay(4);
+	    cal[i] = readReg(i, CALSTS_REG, node); delay(4);
     }
     else if(cal[i] == CAL_DONE){ // Reset sensor
-	res = writeReg(i, CLCTRL_REG, 2); delay(4);
+	res = writeReg(i, CLCTRL_REG, 2, node); delay(4);
 	if(res < 0)
 	    status[i] = res;
 	else{
-	    cal[i] = readReg(i, CALSTS_REG); delay(10);	    
+	    cal[i] = readReg(i, CALSTS_REG, node); delay(10);	    
 	    flag[i] = cal[i];
 	}
     }
@@ -156,19 +49,19 @@ void handleSensor(int i){
 	flag[i] = 0;
     }
     else if(flag[i] == FLAG_OFF){ // Manual Shutdown
-	res = writeReg(i, ONOFF_REG, 0); delay(4);
+	res = writeReg(i, ONOFF_REG, 0, node); delay(4);
 	if(res < 0)
 	    status[i] = res;
 	else
-	    status[i] = readReg(i, STATUS_REG);
+	    status[i] = readReg(i, STATUS_REG, node);
     }
     else if((status[i] == IDLE || status[i] == STANDBY) && flag[i] == FLAG_NONE){ // Turn ON
-	res = writeReg(i, ONOFF_REG, 1); delay(4);
+	res = writeReg(i, ONOFF_REG, 1, node); delay(4);
 	if(res < 0)
 	    status[i] = res;
 	else
-	    status[i] = readReg(i, STATUS_REG); // TODO clear errors
-    }    
+	    status[i] = readReg(i, STATUS_REG, node); // TODO clear errors
+    }
 }
 
 /*
@@ -184,13 +77,13 @@ int getVal(int sensor, char *output, unsigned int cal_out=0){
        
     errval = malloc(5);
     
-    if(flag[sensor]>FLAG_NONE && flag[sensor]<FLAG_OFF ){ // Get calibration status
+    if(flag[sensor]>FLAG_NONE && flag[sensor]<FLAG_OFF){ // Get calibration status
 	strcat(calstr, itoa(cal[sensor], errval, 10));
 	strcpy(output, calstr);
 	retval = 1;
     }
     else if(status[sensor] == ON){    // Get O2 data
-	data = readReg(sensor, O2AVG_REG);
+	data = readReg(sensor, O2AVG_REG, node);
 	if(data < 0){                 // Failed to get data, output ERR
 	    strcat(errstr, itoa(data*-1, errval, 10));
 	    strcpy(output, errstr);	    
@@ -222,26 +115,93 @@ int getVal(int sensor, char *output, unsigned int cal_out=0){
     return retval;
 }
 
-int handleCommands(){
-    int cmd = 0, count = 0, i, garbage;
-    for(i=0; i<8 && Serial.available()>0; i++){
-	cmd = Serial.read();
-	if(i<=3){ // Handle power commands
-	    if(cmd == 48)
-		flag[i] = FLAG_OFF;
-	    else if(cmd == 49)
-		flag[i] = FLAG_NONE;
-	}
-	else if(i>3){ // Handle calibration commands
-	    if(cmd == 49 && flag[i%4] != FLAG_OFF)
-		flag[i%4] = FLAG_CAL;
-	}
-    }
-    while(Serial.available() > 0 && count++<30)
-    	garbage = Serial.read();
+int calibrate(int data){
+    int i;
+    for(i=NUM_SENSORS-1; i>=0; i--)
+	if(((data & (1 << i)) > 0) && status[NUM_SENSORS-1-i]>0)
+	    flag[NUM_SENSORS-1-i] = FLAG_CAL;
     return 1;
 }
 
+int onoff(int data){
+    int i;
+    for(i=NUM_SENSORS-1; i>=0; i--){
+	if((data & (1 << i)) > 0)
+	    flag[NUM_SENSORS-1-i] = FLAG_OFF;
+	else
+	    flag[NUM_SENSORS-1-i] = FLAG_NONE;
+    }
+    return 1;
+}
+
+int bridge(struct cmd _cmd){
+    int i, buf_ptr=0, sensor, regval, buf_sz = 100, retval, rw, handleflag;
+    char *buffer, *locbuf, garbage;
+    buffer = malloc(buf_sz);
+
+    rw = ((int)_cmd.rw)%48;
+    
+    buf_ptr += snprintf(buffer+buf_ptr, buf_sz-buf_ptr, "O10 ");
+    if(rw == 0){ // Read
+	buf_ptr += snprintf(buffer+buf_ptr, buf_sz-buf_ptr, "R ");
+	for(i=NUM_SENSORS-1; i>=0; i--){
+	    if((_cmd.data & (1 << i)) > 0){
+		sensor = NUM_SENSORS-1-i;
+		regval = readReg(sensor, _cmd.reg, node); delay(4);
+		buf_ptr += snprintf(buffer+buf_ptr, buf_sz-buf_ptr,
+				    "%u|%u|%d ",
+				    sensor+1, _cmd.reg, regval);
+	    }
+	}
+	Serial.println(buffer);	
+	free(buffer);
+	Serial.flush();
+	handleflag=0;
+    }
+    else if(rw > 0){ // Write	
+	buf_ptr += snprintf(buffer+buf_ptr, buf_sz-buf_ptr, "W ");
+	sensor=rw-1;
+	retval = writeReg(sensor, _cmd.reg, _cmd.data, node);
+	buf_ptr+=snprintf(buffer+buf_ptr, buf_sz-buf_ptr, "%c|%u|%u|%d ",
+			  _cmd.rw, _cmd.reg, _cmd.data, retval);
+	Serial.println(buffer);	
+	free(buffer);
+	handleflag=1;
+    }
+    return handleflag;
+}
+
+int handleCommands(){
+    int handleflag;
+    char *readval, checksum;
+    struct cmd _cmd;
+    readval = malloc(13);
+    Serial.readBytes(readval, 12);
+    
+    checksum = getChecksum(readval);
+    parseCommand(&_cmd, readval);
+    if(checksum != _cmd.checksum)
+	_cmd.command = 0;
+    switch(_cmd.command){
+    case 0:
+	break;
+    case 1: // Command ID 1: Calibration	
+	calibrate(_cmd.data);
+	break;
+    case 2: // Command ID 2: On/off
+	onoff(_cmd.data);
+	break;
+    case 10: // Command ID 10: Bridge mode
+	handleflag=bridge(_cmd);
+	break;
+    }
+    free(readval);
+    return handleflag;
+}
+//======================================================================
+
+
+//=============================Pressure Funcs===========================
 int get_i2c(int mux_addr, int mux_channel, int device_addr,
 	    int min, int max, char* pressure_out){
 
@@ -274,6 +234,9 @@ int get_i2c(int mux_addr, int mux_channel, int device_addr,
     }           
 }
 
+//======================================================================
+
+
 void setup(){    
     int i, curr_pin, SST_addr;
     
@@ -304,14 +267,13 @@ int k=0, curr_iter = 0;
 
 void loop(){
     char *buffer;
-    
-    if(curr_iter == 0){ // Oxygen data
-	int i, buf_ptr = 0, data, retval, handle_flag[] = {0,0,0,0};    
-	char *output;    
+    //=================================Oxygen operation=============================
+    if(curr_iter == 0){ // Oxygen data       
+	int i, buf_ptr = 0, data, retval, handle_flag[] = {0,0,0,0}, hf, buf_sz = 50;
+	char *buffer, *output;    
 
-	buffer = malloc(50);
-	buf_ptr += snprintf(buffer+buf_ptr, 50-buf_ptr, "O ");
-	
+	buffer = malloc(buf_sz);
+	buf_ptr += snprintf(buffer+buf_ptr, buf_sz-buf_ptr, "OXY ");
 	for(i=0; i<NUM_SENSORS; i++){
 	    output = malloc(20);
 	    if(flag[i] == FLAG_OFF && status[i] == ON){ // Flagged for manual shutdown
@@ -323,12 +285,12 @@ void loop(){
 		handle_flag[i] = 1;
 	    }
 	    else{
-		retval = getVal(i, output, 0); // Not flagged; get data
+		retval = getVal(i, output, 0); // Not flagged; try to get data
 		if(retval < 0)
 		    handle_flag[i] = 1;
 	    }
 	
-	    buf_ptr += snprintf(buffer+buf_ptr, 50-buf_ptr,
+	    buf_ptr += snprintf(buffer+buf_ptr, buf_sz-buf_ptr,
 				" %s ", output);
 	    free(output);
 	    delay(4);
@@ -337,37 +299,40 @@ void loop(){
 	free(buffer);
     
 	if(k==CHK_DELAY && Serial.available()){
-	    handleCommands();
+	    hf=handleCommands();
 	    for(i=0; i< NUM_SENSORS; i++)
-		if(flag[i] == FLAG_CAL || flag[i] == FLAG_OFF)
+		if(flag[i] == FLAG_CAL || flag[i] == FLAG_OFF || hf==1)
 		    handle_flag[i] = 1;	
 	}
     
 	for(i=0; k==CHK_DELAY && i<NUM_SENSORS; i++)
 	    if(handle_flag[i] == 1)
 		handleSensor(i);
-        k+=1;
+    
+	k+=1;
 	k%=(CHK_DELAY+1);
     }
+    //=================================Pressure operation=============================
     else if(curr_iter == 1){
 	char *pressure;
-	int i, retval, buf_ptr = 0;
+	int i, retval, buf_ptr = 0, buf_sz=50;
 
-	buffer = malloc(50);
+	buffer = malloc(buf_sz);
 	pressure = malloc(10);
-    	buf_ptr += snprintf(buffer+buf_ptr, 50-buf_ptr, "P ");
+    	buf_ptr += snprintf(buffer+buf_ptr, buf_sz-buf_ptr, "PRS ");
 	// indexed by 1 to correspond with mux_channel
 	for(i=1; i<MAX_SENSORS; i++){
 	    retval = get_i2c(MUXADDR, i, SLAVE_ADDR,
 			     P_MIN1, P_MAX1, pressure);
-	    buf_ptr += snprintf(buffer+buf_ptr, 50-buf_ptr,
+	    buf_ptr += snprintf(buffer+buf_ptr, buf_sz-buf_ptr,
 				" %s ", pressure);
 	}
+	
 	Serial.println(buffer);
-	Serial.flush();
 	free(buffer);
 	free(pressure);
     }
+    
     curr_iter = ((curr_iter+1)%2);
 }
 
